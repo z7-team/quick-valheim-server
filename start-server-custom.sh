ef1fcffe149cf3abafc40831033cfec41227d558
Tony Rizko <tony@rizkocircle.com>
2021-02-23 19:38:34 -0800

    Update deploy spec to build dockerfile

--- start-server-custom.sh
+++ start-server-custom.sh
@@ -2,13 +2,17 @@
 
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
-export SteamAppId=892970
+export STEAM_APP_ID=892970
+
+WORLD_NAME=${WORLD_NAME:-Dedicated}
+SERVER_NAME=${SERVER_NAME:-My Server}
+PASSWORD=${SERVER_PASS:-secret}
 
 echo "Starting server PRESS CTRL-C to exit"
 
 # Tip: Make a local copy of this script to avoid it being overwritten by steam.
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
 # NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
-./valheim_server.x86_64 -name "Test Z7 server" -port 2456 -world "Aleppo" -password "yallah"
+./valheim_server.x86_64 -nographics -batchmode -name "${SERVER_NAME}" -port 2456 -world "${WORLD_NAME}" -password "${PASSWORD}"
 
 export LD_LIBRARY_PATH=$templdpath
